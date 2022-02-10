c7ed23ee5cb999838978eaa1ab528c230c6cfca2
MASTER0811 <85328244+MASTER0811@users.noreply.github.com>
2022-02-10 15:12:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     img.src = profile.getImageUrl();
     nameprofile.innerHTML = profile.getName();
+    
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); 
 }
