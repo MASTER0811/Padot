38b2e64324a9b773bbbe216de070afe7019ce796
MASTER0811 <85328244+MASTER0811@users.noreply.github.com>
2022-02-20 18:34:10 +0800

    Update sign.js

--- src/Javascript/sign.js
+++ src/Javascript/sign.js
@@ -2,11 +2,7 @@ let signButton = document.getElementById("SignButton"),
     signBox = document.querySelector(".login-container"),
     closeButton = document.querySelector(".closeButton")
 
-let userImg = document.querySelector(".user-profile"),
-    userName = document.querySelector(".user-name")
 
-let signBorder = document.querySelector(".g-signin2"),
-    signOut = document.querySelector(".signOut")
 
 signButton.onclick = () => {
     signBox.classList.add("signNow")
@@ -15,27 +11,22 @@ closeButton.onclick = () =>{
     signBox.classList.remove("signNow")
 }
  
-signBorder.setAttribute("style", "display:flex;")
+let signInButton = document.getElementById("signButton")
+let signOutButton = document.getElementById("signOutButton")
+
+
+
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
 
-    userImg.src = profile.getImageUrl()
-    userName.innerHTML = profile.getName()
-
-    signBorder.setAttribute("style", "display:none;")
-    signOut.setAttribute("style", "display:flex;")
+    signOutButton.style.visibility = "visible"
 }
 
 function signOutBtn() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        alert('User signed out.')
 
-        signBorder.setAttribute("style", "display:flex;")
-        signOut.setAttribute("style", "display:none;")
-
-        userImg.src = "https://cdn-icons-png.flaticon.com/512/149/149071.png"
-        userName.innerHTML = "User"
+        signOutButton.style.visibility = "hidden"
     })
   }
