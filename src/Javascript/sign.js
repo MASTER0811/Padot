6a56460708d0a3f85db90a7843fb56a762e9dc3d
MASTER0811 <85328244+MASTER0811@users.noreply.github.com>
2022-02-20 11:15:44 +0800

    Update sign.js

--- src/Javascript/sign.js
+++ src/Javascript/sign.js
@@ -8,14 +8,14 @@ let userImg = document.querySelector(".user-profile"),
 let signBorder = document.querySelector(".sign"),
     signOut = document.querySelector(".signOut")
 
-
-
 signButton.onclick = () => {
     signBox.classList.add("signNow")
 }
 closeButton.onclick = () =>{
     signBox.classList.remove("signNow")
 }
+ 
+signBorder.setAttribute("style", "display:flex;")
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
@@ -23,21 +23,19 @@ function onSignIn(googleUser) {
     userImg.src = profile.getImageUrl()
     userName.innerHTML = profile.getName()
 
-    signBorder.style.display = "none"
-    signOut.style.display = "flex"
-
-
-    console.log('Image URL: ' + profile.getImageUrl());
+    signBorder.setAttribute("style", "display:none;")
+    signOut.setAttribute("style", "display:flex;")
 }
 
 function signOutBtn() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        alert('User signed out.');
-        signBorder.style.display = "flex"
-        signOut.style.display = "none"
+        alert('User signed out.')
+
+        signBorder.setAttribute("style", "display:flex;")
+        signOut.setAttribute("style", "display:none;")
 
         userImg.src = "https://cdn-icons-png.flaticon.com/512/149/149071.png"
         userName.innerHTML = "User"
-    });
+    })
   }
